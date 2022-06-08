105e6b2a9727e55425962aed6a65d9af7b9612f7
Squalm <43246103+Squalm@users.noreply.github.com>
2022-06-08 19:08:50 +0100

    bug

--- public/script.js
+++ public/script.js
@@ -3,7 +3,7 @@ let base = 6;
 
 const ops = ["+", "-", "*", "/"];
 const ones = ["zero", "one", "two", "three", "four", "five"]
-const sixes = ["six", "douzen", "thirsy", "foursy", "fisy", "nif"]
+const sixes = ["six", "dozen", "thirsy", "foursy", "fisy", "nif"]
 
 let ans = 0;
 let x;
